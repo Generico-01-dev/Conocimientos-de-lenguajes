d5338d2174ee78b8c81afc543acfee0b3b3fc770
Ismael Abellan <ismaelabellan2004@gmail.com>
2026-01-24 18:31:35 +0100

    02_Entrada_usuario

---
+++ c/02_entrada_usuario/entrada_usuario.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+    int edad;
+
+
+    printf("Por favor ingrese su edad: ");
+    scanf("%d", &edad);
+
+
+    printf("Usted ha ingresado: %d\n", edad);
+    return 0;
+}
